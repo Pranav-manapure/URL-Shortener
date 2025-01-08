a1bd7448b91e1c5a883bd35d8bde6323cfa0cb9e
Pranav Sanjay Manapure <manapurepranav@gmail.com>
2025-01-08 11:45:08 +0530

    Adding a model of URL Mapping. A POJO Class for in-memory saving the data in H2Database

--- src/main/java/com/url/shortener/url/shortener/model/URLMapping.java
+++ src/main/java/com/url/shortener/url/shortener/model/URLMapping.java
@@ -1,4 +1,19 @@
 package com.url.shortener.url.shortener.model;
 
 public class URLMapping {
+    private String originalUrl;
+    private String shortUrl;
+
+    public URLMapping(String originalUrl, String shortUrl) {
+        this.originalUrl = originalUrl;
+        this.shortUrl = shortUrl;
+    }
+
+    public String getOriginalUrl() {
+        return originalUrl;
+    }
+
+    public String getShortUrl() {
+        return shortUrl;
+    }
 }
