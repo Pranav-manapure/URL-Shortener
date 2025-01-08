38086f92a38267acad48e04a7c8709a49d8f5011
Pranav Sanjay Manapure <manapurepranav@gmail.com>
2025-01-08 13:15:00 +0530

    In UrlShortenerApplicationTests class: Unit testing for testGenerateShortUrl1 - Validating originalUrl and shortUrl is converted into 8 Charaters of Base64 format or not.

--- src/test/java/com/url/shortener/url/shortener/UrlShortenerApplicationTests.java
+++ src/test/java/com/url/shortener/url/shortener/UrlShortenerApplicationTests.java
@@ -1,13 +1,21 @@
 package com.url.shortener.url.shortener;
 
+import com.url.shortener.url.shortener.util.URLShortenerUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class UrlShortenerApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testGenerateShortUrl1() {
+		String originalUrl = "https://www.linkedin.com/in/pranav-manapure/";
+		String shortUrl = URLShortenerUtil.generateShortUrl(originalUrl);
+
+		assertThat(shortUrl).isNotNull();
+		assertThat(shortUrl.length()).isEqualTo(8);
 	}
 
 }
